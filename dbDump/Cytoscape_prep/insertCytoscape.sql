fb3f4a41a7837b472658b4612d84bad742440a8f
luppala <luppala@unomaha.edu>
2021-04-07 21:06:09 -0500

    Set up Cytoscape database
    The Cytoscape database contains the locations of the network image files for each region being analyzed

---
+++ dbDump/Cytoscape_prep/insertCytoscape.sql
@@ -0,0 +1,6 @@
+INSERT INTO Cytoscape VALUES("Africa","\\networkPlots","AfricaNetworkPlot.png");
+INSERT INTO Cytoscape VALUES("Asia","\\networkPlots","AsiaNetworkPlot.png");
+INSERT INTO Cytoscape VALUES("Europe","\\networkPlots","EuropeNetworkPlot.png");
+INSERT INTO Cytoscape VALUES("North America","\\networkPlots","NAmericaNetworkPlot.png");
+INSERT INTO Cytoscape VALUES("South America","\\networkPlots","SAmericaNetworkPlot.png");
+INSERT INTO Cytoscape VALUES("Oceania","\\networkPlots","OceaniaNetworkPlot.png");
---
+++ dbDump/Cytoscape_prep/makeCytoscape.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Cytoscape(
+	region VARCHAR(20),
+	fileLocation VARCHAR(200),
+	fileName VARCHAR(200),
+	PRIMARY KEY(fileName)
+);
