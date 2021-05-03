6d25a2ab9cca90a81c75d83d5289c113ee80ec3d
luppala <luppala@unomaha.edu>
2021-05-03 02:39:34 -0500

    commented python .csv text processing code

--- dbDump/prepInserts.py
+++ dbDump/prepInserts.py
@@ -1,5 +1,10 @@
+# opens the file to be read
 file = open("allSeqs_quotes.csv", "r")
+
+# reads in the first line of the file
 line = file.readline()
+
+# reads through the file, formatting each line into an insert statement
 while(line):
 	print("INSERT INTO Virus_Variation VALUES(" + line.rstrip() + ");")
 	line = file.readline()
