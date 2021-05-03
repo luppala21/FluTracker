3983313cb01658169abd51d9e2ba8e8bdd62f6ac
luppala <luppala@unomaha.edu>
2021-05-03 02:31:06 -0500

    Delete bioPytranslate.py

--- back-end-code/formatFile/bioPytranslate.py
+++
@@ -1,9 +0,0 @@
-from Bio.Seq import Seq
-from Bio.Alphabet import generic_rna
-
-file = open("testFasta.txt", "r")
-
-rna = Seq(file.read(),generic_rna)
-protein = rna.translate()
-
-print (protein)
