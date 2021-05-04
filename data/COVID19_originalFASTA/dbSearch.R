0923af2880d5ae37625ac258afafe0bf5139ad97
luppala <luppala@unomaha.edu>
2021-05-04 17:56:29 -0500

    Commented dbSearch.R

--- data/COVID19_originalFASTA/dbSearch.R
+++ data/COVID19_originalFASTA/dbSearch.R
@@ -4,6 +4,7 @@ library('igraph')
 gc()
 memory.limit()
 
+# create a file  of the frameshift coocurrences in the fasta file input, which are output as edges
 network<- coocnet(dataFile = "C:/Users/Lavanya Uppala/Documents/GitHub/COVIDTracker/dbSearch_copy/testFasta.fasta", dataType = "DNA", conservativeFilter = 0.95,
                   cooccurFilter = NULL, networkFile = "cooccurNetwork", module = FALSE,
                   moduleFile = "cooccurNetworkModule", property = FALSE,
@@ -11,20 +12,16 @@ network<- coocnet(dataFile = "C:/Users/Lavanya Uppala/Documents/GitHub/COVIDTrac
                   siteCoFile = "siteCooccurr", sampleTimes = 100, debug = FALSE,
                   parallel = FALSE, memory = "sparse")
 
+# save the output file as "cooccurNetwork"
 save(network,filename="cooccurNetwork")
+
+# print the contents of the output file
 print(network$networkFile)
 
 
-data$matrix[1:10,1:10]
-
-networkName = network$xnames
-Network_igraph = toigraph(networkFile=network$networkFile, networkNames=networkName)
-
-
+# Graph the network based on the coocurrence file 
 #cooccurNetwork  = cooc(dataFile=getexample(dataType="protein"),dataType="protein")
 #network_igraph = toigraph(networkFile=cooccurNetwork$networkFile, networkName=c("EPI823725")
-
-                          
 #dataFile=getexample(dataType="protein")
 data = readseq("C:/Users/Lavanya Uppala/Documents/GitHub/COVIDTracker/dbSearch_copy/testFasta.fasta",dataType ="DNA")
 data_process = pprocess(data=data,conservativeFilter=0.95)
